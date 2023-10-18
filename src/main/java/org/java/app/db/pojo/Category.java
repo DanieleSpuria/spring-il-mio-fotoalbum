216d6e2b69840f2a0b4098d3ea4457e0689cc94e
DanieleSpuria <daniele.spuria@proton.me>
2023-10-18 15:19:23 +0200

    create ManyToMany

--- src/main/java/org/java/app/db/pojo/Category.java
+++ src/main/java/org/java/app/db/pojo/Category.java
@@ -1,10 +1,13 @@
 package org.java.app.db.pojo;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 
 
@@ -21,9 +24,14 @@ public class Category {
 	@NotNull
 	private String name;
 	
+	@ManyToMany
+	private List<Photo> photos;
 	
 	
 	
+	
+
+
 	public Category() {}
 	
 	public Category(String name) {
@@ -48,6 +56,17 @@ public class Category {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public List<Photo> getPhotos() {
+		return photos;
+	}
+
+	public void setPhotos(List<Photo> photos) {
+		this.photos = photos;
+	}
+	
+	
+	
 	
 	@Override
 	public String toString() {
