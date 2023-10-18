aece213babdbf15a20cf1b0d5feee7ea47d06793
DanieleSpuria <daniele.spuria@proton.me>
2023-10-18 16:35:03 +0200

    add show() photo

--- src/main/java/org/java/app/mvc/controller/PhotoController.java
+++ src/main/java/org/java/app/mvc/controller/PhotoController.java
@@ -1,6 +1,7 @@
 package org.java.app.mvc.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.java.app.db.pojo.Photo;
 import org.java.app.db.serv.PhotoServ;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -23,7 +25,7 @@ public class PhotoController {
 	
 	
 	@GetMapping
-	public String getIndex(
+	public String index(
 				@RequestParam(required = false, name = "search") String search,
 				Model model
 			) {
@@ -36,5 +38,20 @@ public class PhotoController {
 		model.addAttribute("search", search);
 		
 		return "photo/index";
+	}
+	
+	
+	
+	
+	@GetMapping("/{id}")
+	public String show(
+				@PathVariable int id,
+				Model model
+			) {
+		
+		Optional<Photo> photo = photoServ.findById(id);
+		model.addAttribute("photo", photo.get());
+		
+		return "photo/show";
 	}
 }
